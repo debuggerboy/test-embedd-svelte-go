fd4d91b368b4f79c376e92693520710ceb4c03bb
Anish Asokan <anish@cassandra.pulsiot.com>
2024-12-08 04:53:01 +0530

    Update to sveltekit 5

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -3,6 +3,8 @@ import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
   kit: {
     adapter: adapter({
       pages: "./../server/uibuild",
